460f8fe4852b6ae81e1d08df9919f8ceb5176e67
unknown <a.mickens5@gmail.com>
2021-11-17 19:54:19 -0800

    first commit

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Ashley Mickens" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
@@ -14,10 +14,54 @@ document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 document.getElementById('add-gb').addEventListener('click', function() {
+   
     // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+    console.log('add-gb')
 
     // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
 })
 
 // TODO: Hook up event listeners for the rest of the buttons
+
+var button = document.getElementById('add-gb'),
+count = 0
+button.onclick = function () {
+    count ++;
+    button.innerHTML = count;    
+
+}
+
+var button = document.getElementById('minus-gb'),
+count = 0
+button.onclick = function () {
+    count --;
+    button.innerHTML = count;
+}
+
+var button = document.getElementById('add-cc'),
+count = 0
+button.onclick = function () {
+    count ++;
+    button.innerHTML = count;
+}
+
+var button = document.getElementById('add-sugar'),
+count = 0
+button.onclick = function () {
+    count ++;
+    button.innerHTML = count;
+}
+
+var button = document.getElementById('minus-cc'),
+count = 0
+button.onclick = function () {
+    count --;
+    button.innerHTML = count;
+}
+
+var button = document.getElementById('minus-sugar'),
+count = 0
+button.onclick = function () {
+    count --;
+    button.innerHTML = count;
+}
